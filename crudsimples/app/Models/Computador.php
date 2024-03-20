da1f6104f6ad57006f2e46db1153a938235f9ff0
Isaah <isaaholiveira15@gmail.com>
2024-03-20 13:50:25 -0300

    feat: crud simples

---
+++ crudsimples/app/Models/Computador.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Computador extends Model
+{
+    use HasFactory;
+    protected $fillable = ['id', 'marca', 'preco', 'ram', 'disco'];
+}
